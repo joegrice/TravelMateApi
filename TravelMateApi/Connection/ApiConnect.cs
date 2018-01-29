fafb776f1048c4b1e1719a52eef49ca2f1cbcf9c
Joe Grice <joe.grice@hotmail.co.uk>
2018-01-29 17:50:50 +0000

    Added searching journeys

---
+++ TravelMateApi/Connection/ApiConnect.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TravelMateApi.Connection
+{
+    public class ApiConnect
+    {
+        public async Task<string> GetJson(string url)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url + Credentials.Tfl);
+            var result = await response.Content.ReadAsStringAsync();
+
+            return result;
+        }
+    }
+}
---
+++ TravelMateApi/Controllers/JourneyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelMateApi.Journey;
+
+namespace TravelMateApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class JourneyController : Controller
+    {
+        [HttpGet("search/{startlocation}/{endlocation}")]
+        public string Search(string startlocation, string endlocation)
+        {
+            var getJourney = new GetJourney(startlocation, endlocation);
+            var resultSearchJourney = getJourney.SearchJourney();
+            return resultSearchJourney;
+        }
+    }
+}
