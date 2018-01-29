21c9f0129814a1353d16e80e398b702324399bff
Joe Grice <joe.grice@hotmail.co.uk>
2018-01-29 18:12:15 +0000

    Added start of select journey

--- TravelMateApi/Controllers/JourneyController.cs
+++ TravelMateApi/Controllers/JourneyController.cs
@@ -9,8 +9,16 @@ namespace TravelMateApi.Controllers
         [HttpGet("search/{startlocation}/{endlocation}")]
         public string Search(string startlocation, string endlocation)
         {
-            var getJourney = new GetJourney(startlocation, endlocation);
-            var resultSearchJourney = getJourney.SearchJourney();
+            var getJourney = new SearchJourney(startlocation, endlocation);
+            var resultSearchJourney = getJourney.Search();
+            return resultSearchJourney;
+        }
+
+        [HttpGet("select/{pos}/{startlocation}/{endlocation}")]
+        public string Search(int pos, string startlocation, string endlocation)
+        {
+            var getJourney = new SelectJourney(pos, startlocation, endlocation);
+            var resultSearchJourney = getJourney.Select();
             return resultSearchJourney;
         }
     }
---
+++ TravelMateApi/Journey/SelectJourney.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using TravelMateApi.Connection;
+using TravelMateApi.Models;
+
+namespace TravelMateApi.Journey
+{
+    public class SelectJourney {
+
+        private readonly string BaseUrl = "https://api.tfl.gov.uk";
+        private readonly int _pos;
+        private readonly string _startLocation;
+        private readonly string _endLocation;
+
+        public SelectJourney(int pos, string startLocation, string endLocation) {
+            _pos = pos;
+            _startLocation = startLocation;
+            _endLocation = endLocation;
+        }
+
+        public string Select() {
+            var apiConnect = new ApiConnect();
+            var url = $@"{BaseUrl}/Journey/JourneyResults/{_startLocation}/to/{_endLocation}";
+            var json = apiConnect.GetJson(url);
+            var result = JsonConvert.DeserializeObject<JourneySearch>(json.Result);
+            var selectedResult = result.Journeys[_pos];
+            var serializeObject = JsonConvert.SerializeObject(selectedResult);
+            return serializeObject;
+        }
+    }
+}
